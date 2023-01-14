cf8aa418aaf2c7dda36f36cf4205fbb31af93b58
Jishnu075 <jishnubiju075@gmail.com>
2023-01-14 22:02:27 +0530

    completed with 2 pages

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const allProfiles = document.querySelector(".all-profiles");
 const statusMsg = document.querySelector(".status-msg");
 const page1_btn = document.getElementById("page-btn-1");
 const page2_btn = document.getElementById("page-btn-2");
+const loading = document.querySelector(".loading");
 
 function getResponse(page = "per_page=6") {
   // to empty all arrays
@@ -19,7 +20,6 @@ function getResponse(page = "per_page=6") {
   const url = `https://reqres.in/api/users?${page}&` + Math.random(2);
   const req = new XMLHttpRequest();
   req.open("GET", url);
-  req.send();
   req.onload = function () {
     const responseData = req.responseText;
     const jsonResponse = JSON.parse(responseData);
@@ -31,14 +31,21 @@ function getResponse(page = "per_page=6") {
     });
     displayProfile();
   };
+  req.send();
 
   // func for handling if it was not able to connect to the network
   req.onreadystatechange = function () {
-    if (req.readyState === 4) {
+    if (req.readyState === 1 || req.readyState === 2 || req.readyState === 3) {
+      loadingTime();
+    } else if (req.readyState === 4) {
       if (req.status === 200) {
         statusMsg.textContent = "Welcome";
+        loading.classList.add("hidden");
+        page1_btn.classList.remove("hidden");
+        page2_btn.classList.remove("hidden");
       } else {
         statusMsg.classList.add("red");
+        loading.classList.add("hidden");
         statusMsg.textContent = `Something went wrong, Try again later:(`;
       }
     }
@@ -63,8 +70,16 @@ function displayProfile() {
 getResponse();
 
 page1_btn.addEventListener("click", function () {
+  loadingTime();
   getResponse("page=1");
 });
 page2_btn.addEventListener("click", function () {
+  loadingTime();
   getResponse("page=2");
 });
+
+function loadingTime() {
+  page1_btn.classList.add("hidden");
+  page2_btn.classList.add("hidden");
+  loading.classList.remove("hidden");
+}
